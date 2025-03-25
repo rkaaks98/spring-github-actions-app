74fcd7657fcb1928b59579b1aea2906e9abb8e9d
wnstj050505 <wnstj050505@gmail.com>
2025-03-25 13:46:43 +0900

    commit 0.0.1

---
+++ src/test/java/kr/co/wnstj050505/springgithubactionsapp/SpringGithubActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wnstj050505.springgithubactionsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
